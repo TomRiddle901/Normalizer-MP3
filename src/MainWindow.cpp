154f6a1c8e780a480128165c18fb56c4e153e411
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-09 13:05:14 +0100

    Implemented class for normalization task and reveling peak

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -21,52 +21,37 @@
 #include <iostream>
 #include <atomic>
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
-{
-    auto central = new QWidget(this);
-    auto layout = new QVBoxLayout(central);
+// --- Task per normalizzazione ---
+class NormalizeTask : public QRunnable {
+public:
+    NormalizeTask(const QString &in, const QString &out, double peak, const QStringList &params,
+                  std::function<void(bool,const QString&)> callback)
+        : inputFile(in), outputFile(out), targetPeak(peak), ffmpegParams(params), cb(callback) {}
 
-    statusLabel = new QLabel("Normalizzatore MP3 pronto!", this);
-    logText = new QTextEdit(this);
-    logText->setReadOnly(true);
+    void run() override{
+        bool success = false;
+        QString errorMsg;
+        QString tempFile = outputFile + ".tmp.mp3";
 
-    normalizeButton = new QPushButton("Seleziona e normalizza MP3", this);
+        QString program = "./thirdparty/ffmpeg/ffmpeg";
 
-    layout->addWidget(statusLabel);
-    layout->addWidget(logText);
-    layout->addWidget(normalizeButton);
+        // Rilevazione picco
+        QStringList cmdPeak = {"-i", inputFile, "-map", "0:a:0?", "-af", "volumedetect", "-f", "null", "-"};
+        QProcess peakProc;
+        peakProc.start(program, cmdPeak);
+        peakProc.waitForFinished(-1);
+        QString stderr = peakProc.readAllStandardError();
 
-    setCentralWidget(central);
-    setWindowTitle("Normalizzatore MP3");
-    resize(500, 300);
+        double currentPeak = 0.0;
+        std::regex rx("max_volume:\\s*([-\\d.]+)");
+        std::smatch match;
+        std::string s = stderr.toStdString();
+        if (std::regex_search(s, match, rx)){
+            currentPeak = std::stod(match[1]);
+        }else{
+            errorMsg = "Impossibile trovare max_volume";
+        }
 
-    connect(normalizeButton, &QPushButton::clicked, this, &MainWindow::onNormalizeClicked);
-}
-
-void MainWindow::onNormalizeClicked()
-{
-    QString file = QFileDialog::getOpenFileName(this, "Seleziona MP3", "", "MP3 Files (*.mp3)");
-    if (file.isEmpty()){return;}
-
-    logText->append("File selezionato: " + file);
-    statusLabel->setText("Normalizzazione in corso...");
-
-    // Chiamata ffmpeg per normalizzare il volume
-    QString outputFile = file;
-    outputFile.insert(file.lastIndexOf('.'), "_normalized");
-
-    QString program = "./thirdparty/ffmpeg/ffmpeg"; // Persorso locale
-    QStringList arguments;
-    arguments << "-i" << file << "-filter:a" << "loudnorm" << outputFile;
-
-    QProcess ffmpeg;
-    ffmpeg.start(program, arguments);
-    if (!ffmpeg.waitForFinished()){
-        logText->append("Errore nella normalizzazione");
-        statusLabel->setText("Errore!");
-        return;
+        double gain = targetPeak - currentPeak;
     }
-
-    logText->append("File normalizzato salvato come: " + outputFile);
-    statusLabel->setText("Normalizzazione completata!");
-}
+};
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -6,7 +6,7 @@
 #include <QAtomicInt>
 #include <QStringList>
 
-QT_BEGIN_NAMESPACE;
+QT_BEGIN_NAMESPACE
 class QPushButton;
 class QLabel;
 class QTextEdit;
@@ -14,52 +14,51 @@ class QProgressBar;
 class QLineEdit;
 class QComboBox;
 class QCheckBox;
-QT_END_NAMESPACE;
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
 
-    public:
-        MainWindow(QWidget *parent = nullptr);
+private slots:
+    void browseInput();
+    void browseOutput();
+    void startNormalization();
+    void stopNormalization();
 
-    private slots:
-        void browseInput();
-        void browseOutput();
-        void startNormalization();
-        void stopNormalization();
+private:
+    void logMessage(const QString &msg, const QColor &color = Qt::black);
+    void updateProgress();
+    void processFiles();
+    bool normalizeSingleFile(const QString &inputFile, const QString &outputFile, double targetPeak, const QStringList &ffmpegAudioParams);
+    void copyID3Tags(const QString &src, const QString &dst);
 
-    private:
-        void logMessage(const QString &msg, const QColor &color = Qt::black);
-        void updateProgress();
-        void processFiles();
-        void normalizeSingleFile(const QString &inputFile, const QString &outputFile, double targetPeak, const QStringList &ffmpegAudioParams);
-        void copyID3Tags(const QString &src, const QString &dst);
+    // GUI
+    QLineEdit *inputEntry;
+    QLineEdit *outputEntry;
+    QLineEdit *peakEntry;
+    QComboBox *qualityCombo;
+    QCheckBox *overwriteCheck;
+    QPushButton *inputButton;
+    QPushButton *outputButton;
+    QPushButton *startButton;
+    QPushButton *stopButton;
+    QLabel *statusLabel;
+    QTextEdit *logText;
+    QProgressBar *progressBar;
 
-        // GUI
-        QLineEdit *inputEntry;
-        QLineEdit *outputEntry;
-        QLineEdit *peakEntry;
-        QComboBox *qualityCombo;
-        QCheckBox *overwriteCheck;
-        QPushButton *inputButton;
-        QPushButton *outputButton;
-        QPushButton *startButton;
-        QPushButton *stopbutton;
-        QLabel *statusLabel;
-        QTextEdit *logText;
-        QProgressBar *progressBar;
+    // Stato
+    QString inputDirPath;
+    QString outputDirPath;
+    double targetPeak = 0.0;
+    QString quality;
+    bool overwrite = false;
+    bool isRunning = false;
+    bool stopFlag = false;
 
-        // Stato
-        QString inputDirPath;
-        QString outputDirPath;
-        double targetPeak = 0.0;
-        QString quality;
-        bool overwrite = false;
-        bool isRunning = false;
-        bool stopFlag = false;
-
-        QStringList mp3Files;
-        QAtomicInt processedfiles;
-        QStringList failedFiles;
+    QStringList mp3Files;
+    QAtomicInt processedFiles;
+    QStringList failedFiles;
 };
