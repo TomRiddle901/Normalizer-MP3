60ed2de0480df4c1318e103d670cbadc389aa853
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-09 10:26:12 +0100

    Added settings to IntelliSense of vscode (avoid #include errors)

---
+++ build/CMakeFiles/Normalizzatore.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Normalizzatore_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Normalizzatore_autogen.dir/ParseCache.txt"
+  "Normalizzatore_autogen"
+  "CMakeFiles/Normalizzatore.dir/Normalizzatore_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Normalizzatore.dir/Normalizzatore_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Normalizzatore.dir/src/MainWindow.cpp.o"
+  "CMakeFiles/Normalizzatore.dir/src/MainWindow.cpp.o.d"
+  "CMakeFiles/Normalizzatore.dir/src/main.cpp.o"
+  "CMakeFiles/Normalizzatore.dir/src/main.cpp.o.d"
+  "Normalizzatore"
+  "Normalizzatore.pdb"
+  "Normalizzatore_autogen/mocs_compilation.cpp"
+  "Normalizzatore_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Normalizzatore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
